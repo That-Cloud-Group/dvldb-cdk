639bc1cd3bb678e41591e2815f5bb70b36118a41
Richard Julian <richard@rjulian.net>
2023-05-10 23:50:50 +0200

    Add in root user configuration k8s code

---
+++ k8s/root-user-container/typescript/main.ts
@@ -0,0 +1,48 @@
+import { Construct } from 'constructs';
+import { App, Chart, ChartProps } from 'cdk8s';
+
+// imported constructs
+import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+
+
+export class RootContainerChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+    super(scope, id, props);
+
+    const label = { app: 'root-user-container' };
+
+    new KubeService(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'nginx',
+                image: 'nginx:latest',
+                ports: [ { containerPort: 8080 } ],
+                securityContext: { runAsUser: 0 }
+              }
+            ]
+          }
+        }
+      }
+    });
+  }
+}
+
+const app = new App();
+new RootContainerChart(app, 'attackstacks-root-user-container');
+app.synth();
