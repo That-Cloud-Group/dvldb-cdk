e70259ad74b8ac2311f7b8340136387c08b3602e
Richard Julian <richard@rjulian.net>
2023-06-25 21:05:17 +0200

    Add in simple gcp public bucket

---
+++ gcp/gcp-public-bucket/main.ts
@@ -0,0 +1,57 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import { GoogleProvider } from "./.gen/providers/google/provider";
+import {
+  StorageBucket,
+  StorageBucketConfig,
+} from "./.gen/providers/google/storage-bucket";
+import {
+  StorageBucketIamPolicy,
+  StorageBucketIamPolicyConfig,
+} from "./.gen/providers/google/storage-bucket-iam-policy";
+import {
+  DataGoogleIamPolicy,
+  DataGoogleIamPolicyConfig,
+  DataGoogleIamPolicyBinding,
+} from "./.gen/providers/google/data-google-iam-policy";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new GoogleProvider(this, "Google", {
+      region: "us-central1",
+      zone: "us-central1-c",
+    });
+
+    const bucketConfig: StorageBucketConfig = {
+      name: "PublicBucket",
+      location: "us-central1-c",
+      publicAccessPrevention: "false",
+    };
+
+    const publicBucket = new StorageBucket(this, "PublicBucket", bucketConfig);
+
+    const policyBinding: DataGoogleIamPolicyBinding = {
+      members: ["allUsers"],
+      role: "roles/storage.objectViewer",
+    };
+    const policyConfig: DataGoogleIamPolicyConfig = {
+      binding: [policyBinding],
+    };
+    const publicIamPolicy = new DataGoogleIamPolicy(
+      this,
+      "PublicPolicy",
+      policyConfig
+    );
+    const bucketPolicyConfig: StorageBucketIamPolicyConfig = {
+      bucket: publicBucket.name,
+      policyData: publicIamPolicy.policyData,
+    };
+    new StorageBucketIamPolicy(this, "BucketPolicy", bucketPolicyConfig);
+  }
+}
+
+const app = new App();
+new MyStack(app, "gcp-public-bucket");
+app.synth();
